ab4e5accff090a17121b6b3a051c0be84fde7815
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-01-29 23:27:27 -0500

    Assignment-01 .change()
    22.01.29 11:27

--- WEB303As01/script.js
+++ WEB303As01/script.js
@@ -1,5 +1,21 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	{Hyun-jun, Lee 0770897}
 */
 
+$(document).ready(function () {
+
+	$('fieldset').change(function () {
+
+		//console.log('fieldset Changed');
+
+		let $salaryVal = $('#yearly-salary').val();
+		let $percnetVal = $('#percent').val();
+		let $amount = $salaryVal * $percnetVal / 100;
+
+		console.log('$salaryVal * $percnetVal / 100');
+
+		$('span#amount').text('$' + $amount.toFixed(2));
+
+	});
+});
