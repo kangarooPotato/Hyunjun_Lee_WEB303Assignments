6e577675ca654e8aa59ecefad5cc84d5daa58256
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-02-06 22:05:00 -0500

    Assignment-02_Final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
 // WEB303 Assignment 2
+// Hyun-jun Lee (0770897)
 
+$(function () {
+	
+	let xhr = new XMLHttpRequest;
+	let conBox = document.getElementById('content');
 
+	$('#prospect').click(function () {
+		$("div#content").html("").hide().fadeIn();
+		
+		xhr.open('GET', 'prospect.html', true);
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			conBox.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send(null);
+	});
+
+	$('#convert').click(function () {
+		xhr.open('GET', 'convert.html', true);
+		$("div#content").html("").hide().fadeIn();
+	
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			conBox.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send(null);
+	});
+
+	$('#retain').click(function () {
+		xhr.open('GET', 'retain.html', true);
+		$("div#content").html("").hide().fadeIn();
+
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			conBox.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send(null);
+	});
+
+});
