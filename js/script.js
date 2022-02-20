494be1dfbaf92bcfec65acb3f3a6ef72193c96d5
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-02-20 10:19:10 -0500

    Assignment-03_tried after the due date

--- js/script.js
+++ js/script.js
@@ -4,39 +4,40 @@
 let teamJson = $.getJSON("team.json");
 
 let teamAjax = $.ajax({
-	url: "team.json",
+	url: 'team.json',
+	type: 'GET',
 	datatype: 'json',
-	type: "GET",
-	success: function () {
-		$('#team').append("Loding...");
-		console.log('success');
-			  }
+	success: $('#team').append(`<div>Loding...<div>`)
 });
 
 $(function () {
+	console.log("ready!");
 
 //	teamJson.done(function (data) {
-//        
-//        $.each(data.members, function (key, val) {
-//            console.log("data key: ", key, " and data value: ", val);
+//		$.each(data.members, function (key, val) {
+//			console.log("data key: ", key, " and data value: ", val);
 //
-//            $('#team').append(`<h2 class="name">${val.name}</h2>
+//		$('#team').append(`<h2 class="name">${val.name}</h2>
 //                <h5 class="position">${val.position}</h5>
 //                <p class="bio">${val.bio}</p></br>
 //            </div>`);
-//        });
-//    });
-	
-teamAjax.done(function (data) {
-	
-	$.each(data.members, function (key, val) {
+//		});
+//	});
 
-            $('#team').append(`<h2 class="name">${val.name}</h2>
+	setTimeout(function () {
+		teamAjax.done(function (data) {
+			$('#team').html("");
+
+			$.each(data.members, function (key, val) {
+				console.log("data key: ", key, " and data value: ", val);
+
+				$('#team').append(`<h2 class="name">${val.name}</h2>
                 <h5 class="position">${val.position}</h5>
                 <p class="bio">${val.bio}</p>
             </div></br>`);
-        });
-	
+			});
+		});
 
-	});
+	}, 3000);
+
 });
