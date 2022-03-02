2b29b2d8145b57f1304e1d2232092db55f361da2
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-03-02 11:24:52 -0500

    1

--- WEB303As05/script.js
+++ WEB303As05/script.js
@@ -1,10 +1,36 @@
 /*
     Assignment 05
+	Hyun-jun Lee 0770897
 */
 
 $(document).ready(function () {
     // your code here
+	
+	class ContentItem {
+    // animalType;
+    constructor(id, name, description, category, genre) {
+        this.conId = id;
+		this.conName = name;
+		this.conDes ;
+        this.conCat = category;
+		this.conGen = 
+    }
+    toString() {
+        return "This animal is a " + this.animalType;
+    }
+}
 
+
+let rabbit = new Animal("Rabbit","long");
+let rabbitString = `The animal says: ${rabbit}`;
+// note: console.log doesn't invoke toString
+console.log(rabbit);
+console.log(rabbitString);
+	
+	
+	
+	
+	
 });
 
 
