ae799f15ecbf0503090d1505b869b1450b008c67
KangarooPotato <w0770897@myscc.ca>
2022-04-03 23:52:14 -0400

    4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-$(function(){
-	
-	
-	
+$(function () {
+    $.ajax({
+        url: "characters.json",
+        dataType: "json",
+        type: "get",
+        success: function (data) {
+            $(data.MCU).each(function (key, val) {
+                $('#datathead').append(`
+                <tr>
+                    <td>${val.heroName}</td>
+                    <td>${val.species}</td>
+                    <td>${val.gender}</td>
+                    <td>${val.fname}</td>
+                    <td>${val.lname}</td>
+                </tr>`);
+            });
+        },
+        error: function () {
+             console.log('error');
+        }
+    });
 });
