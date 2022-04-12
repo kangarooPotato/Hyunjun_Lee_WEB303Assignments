f8a8bfa57bab33e820b5fced25f1d3920053acbf
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-04-12 13:53:43 -0400

    new 02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 $(function () {
 
     var $tbody = $('tbody'); // reference <tbody> element
+    var $search = $('#filter-search'); // Get input
+    var cache = [
+        // example of how the cache gets built
+        // {
+        //     element: $row0Reference,
+        //     text: "magnus"
+        // },
+        // {
+        //     element: $row1Reference,
+        //     text: "ding"
+        // }
+    ]; // Create array
 
     $.getJSON("MCUheros.json").done((data) => {
         // jQuery.each of the players in the array
@@ -17,11 +29,38 @@ $(function () {
 
             $tbody.append($row); // Add row to the tbody
 
-            // rows.push({ // Create rows array
-            //     person: person, // Person object
-            //     $element: $row // jQuery object: row
-            // });
+            cache.push({ // Create rows array
+                element: $row, // Reference to the row element
+                text: val.fname.trim().toLowerCase() // The text we're searching against (which in this case is first name)
+            });
         });
     });
-
+    // $imgs.each(function () { // Each img
+    //     cache.push({ // Add to cache
+    //         element: this, // This image
+    //         text: this.alt.trim().toLowerCase() // Its alt text
+    //     });
+    // });
+    function filter() {
+        var query = this.value.trim().toLowerCase(); // Get query
+        if (query) { // If there’s a query
+            cache.forEach(function (marvleHeros) { // Each cache entry
+                var index = 0; // Set index to 0
+                index = marvleHeros.text.indexOf(query); // Is text in there?
+                if (index != -1) { // we found the string in their first navs.element.addClass("found-first-name"); // we will apply colours based on this class
+                }
+                else { // player firs tname doesn't have the query string, make sure it's not higlighted
+                    marvleHeros.element.removeClass("found-first-name")
+                }
+            });
+        }
+    }
+    if ('oninput' in $search[0]) {
+        // Use input event to call filter()
+        $search.on('input', filter);
+    } else { // Otherwise
+        // Use keyup event to call filter()
+        $search.on('keyup', filter);
+    }
 });
+
